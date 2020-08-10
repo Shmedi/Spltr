6263bf7ce21c1a199e69c26b3dafdeddca8ad1ee
Shmedi <eduard.tupy@hotmail.com>
2020-08-10 16:43:23 -0400

    Updates

---
+++ precache-manifest.15837d72715f3f04480305b4d70f4c95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bd1930c429f6d8ed5f583cded1d740",
+    "url": "/eduardTupyProjectFive/index.html"
+  },
+  {
+    "revision": "0ffa744ade831869a914",
+    "url": "/eduardTupyProjectFive/static/css/main.f0f205b6.chunk.css"
+  },
+  {
+    "revision": "93acd452c4d00b8ffa9c",
+    "url": "/eduardTupyProjectFive/static/js/2.b9a90f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eduardTupyProjectFive/static/js/2.b9a90f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffa744ade831869a914",
+    "url": "/eduardTupyProjectFive/static/js/main.adce921d.chunk.js"
+  },
+  {
+    "revision": "2a571d178dbbeaac91f1",
+    "url": "/eduardTupyProjectFive/static/js/runtime-main.9fcc0ab8.js"
+  }
+]);
